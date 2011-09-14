18d945cdc6bf88b1c2d50593250dbd37c28c286e
Greg Heartsfield <scsibug@imap.cc>
2011-09-13 19:50:22 -0500

    Open device directly.

--- msp430_serial.c
+++ msp430_serial.c
@@ -1,6 +1,7 @@
 #include "msp430_serial.h"
 #include <stdio.h>
-#include <libusb-1.0/libusb.h> 
+#include <errno.h>
+#include <libusb-1.0/libusb.h>
 
 bool isLibUSBInitialized = false;
 struct libusb_context *mspContext = NULL;
@@ -8,7 +9,14 @@ struct libusb_context *mspContext = NULL;
 int main() {
   printf("msp430_serial starting...\n");
   printf("libusb initing...\n");
-  init_dev(1, 0, 0xf432);
+  // initialize USB context, and get a device handle.
+  HANDLE h = init_dev(1, 0, 0xf432);
+  if (h == NULL) {
+      fprintf(stderr, "Handle is NULL, initialization failed.\n");
+      return(1);
+  }
+  // enable debugging
+  libusb_set_debug(mspContext, 3);
   printf("Exiting.\n");
   return(0);
 }
@@ -17,14 +25,14 @@ int main() {
 HANDLE init_dev(UINT DevNum, unsigned int dwReserved, unsigned long ProductID) {
   void * handle = NULL;
   //struct libusb_device_handle *devh = NULL;
-  //libusb_device **devs;
-  //ssize_t cnt;
+  libusb_device **devs;
+  ssize_t cnt;
   int r = 1;
-  //libusb_device *dev;
-  //unsigned int i = 0;
+  libusb_device *dev;
+  unsigned int i = 0;
   //unsigned int mspFoundCount = 0;
-  //struct libusb_device_descriptor desc;
-  
+  struct libusb_device_descriptor desc;
+
   if (!isLibUSBInitialized) {
     r = libusb_init(&mspContext);
     if (r < 0) {
@@ -35,6 +43,85 @@ HANDLE init_dev(UINT DevNum, unsigned int dwReserved, unsigned long ProductID) {
     }
     isLibUSBInitialized = true;
   }
+  // directly open the first device with a matching vendor/product ID
+  handle = libusb_open_device_with_vid_pid(mspContext, 1105, 62514);
   return handle;
 }
 
+static void MSP_uninitialize(void) {
+  if (isLibUSBInitialized) {
+    libusb_exit(mspContext);
+    mspContext = NULL;
+    isLibUSBInitialized = false;
+  }
+}
+
+static int MSP_libusbError(int r) {
+  switch (r) {
+  case LIBUSB_SUCCESS:
+    // No error
+    return 0;
+    break;
+  case LIBUSB_ERROR_IO:
+    fprintf(stderr, "libusb error: LIBUSB_ERROR_IO\n");
+    errno = EIO;
+    break;
+  case LIBUSB_ERROR_INVALID_PARAM:
+    fprintf(stderr, "libusb error: LIBUSB_ERROR_INVALID_PARAM\n");
+    errno = EINVAL;
+    break;
+  case LIBUSB_ERROR_ACCESS:
+    fprintf(stderr, "libusb error: LIBUSB_ERROR_ACCESS\n");
+    errno = EACCES;
+    break;
+  case LIBUSB_ERROR_NO_DEVICE:
+    fprintf(stderr, "libusb error: LIBUSB_ERROR_NO_DEVICE\n");
+    errno = ENXIO;
+    break;
+  case LIBUSB_ERROR_NOT_FOUND:
+    fprintf(stderr, "libusb error: LIBUSB_ERROR_NOT_FOUND\n");
+    errno = ENOENT;
+    break;
+  case LIBUSB_ERROR_BUSY:
+    fprintf(stderr, "libusb error: LIBUSB_ERROR_BUSY\n");
+    errno = EBUSY;
+    break;
+  case LIBUSB_ERROR_TIMEOUT:
+    fprintf(stderr, "libusb error: LIBUSB_ERROR_TIMEOUT\n");
+    errno = ETIMEDOUT;
+    break;
+  case LIBUSB_ERROR_OVERFLOW:
+    fprintf(stderr, "libusb error: LIBUSB_ERROR_OVERFLOW\n");
+    errno = EOVERFLOW;
+    break;
+  case LIBUSB_ERROR_PIPE:
+    fprintf(stderr, "libusb error: LIBUSB_ERROR_PIPE\n");
+    errno = EPIPE;
+    break;
+  case LIBUSB_ERROR_INTERRUPTED:
+    fprintf(stderr, "libusb error: LIBUSB_ERROR_INTERRUPTED\n");
+    errno = EINTR;
+    break;
+  case LIBUSB_ERROR_NO_MEM:
+    fprintf(stderr, "libusb error: LIBUSB_ERROR_NO_MEM\n");
+    errno = ENOMEM;
+    break;
+  case LIBUSB_ERROR_NOT_SUPPORTED:
+    fprintf(stderr, "libusb error: LIBUSB_ERROR_NOT_SUPPORTED\n");
+    errno = ENOSYS;
+    break;
+  case LIBUSB_ERROR_OTHER:
+    fprintf(stderr, "libusb error: LIBUSB_ERROR_OTHER\n");
+    if (errno == 0) {
+      errno = ENOSYS;
+    }
+    break;
+  default:
+    fprintf(stderr, "libusb error: Unexpected error code: %d.\n", r);
+    if (errno == 0) {
+      errno = ENOSYS;
+    }
+    break;
+  }
+  return -1;
+}
--- msp430_serial.h
+++ msp430_serial.h
@@ -6,3 +6,5 @@ typedef unsigned int UINT;
 typedef unsigned char BYTE;
 
 HANDLE init_dev(UINT DevNum, unsigned int dwReserved, unsigned long ProductID);
+static int MSP_libusbError(int r);
+static void MSP_uninitialize(void);
